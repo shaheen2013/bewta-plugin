a62cc58b2197eee02ef8291482a0b4f592d5058d
shuvo <saifulshuvo2@gmail.com>
2025-05-12 15:09:38 +0600

    first commit

---
+++ bewta-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Bewta Plugin
+Plugin URI: https://mediusware.com/
+Description: Used by millions.
+Version: 5.3.7
+Requires at least: 5.8
+Requires PHP: 5.6.20
+Author: Mediusware Ltd.
+Author URI: https://mediusware.com/
+License: GPLv2 or later
+Text Domain: bewta-plugin
+*/
